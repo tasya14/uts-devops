899aea8344ac74dc1ef107e1a6b91faa846e54cd
tasya14 <alvindanatassya@gmail.com>
2025-05-22 17:16:29 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx_terraform"
+  image = docker_image.nginx.name
+  ports {
+    internal = 90
+    external = 9090
+  }
+}
